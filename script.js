bd6aa916e97a50b390b2502cf66fb884edd148b8
morganpaiine <mjpaine@live.ca>
2023-11-29 22:38:38 -0500

    phew

--- script.js
+++ script.js
@@ -19,33 +19,47 @@ let appContainer = document.getElementById(appID);
 // Functions
 
 function addTask() {
-  // Get the input value
   const input = document.getElementById("todo-input");
   const inputValue = input.value;
 
-  // If the input value is empty then end
+ 
   if (!inputValue) {
     console.error("Error: No input value");
     return;
   }
 
-  // Create a new list item
+
   const listItem = document.createElement("li");
   listItem.innerText = inputValue;
 
 
-  // Add the list item to the list
+  listItem.addEventListener("click", () =>{
+    listItem.classList.toggle("completed");
+  });
+
   const list = document.getElementById("todo-list");
   list.appendChild(listItem);
 
   // Clear the input
   input.value = "";
 }
+function removeCompletedTasks() {
+  const completedTasks = document.querySelectorAll(".completed");
+  completedTasks.forEach((task) => {
+    task.remove();
+  });
+}
+
+
+
 //
+
+
 
 // Add a heading to the app container
 function inititialise() {
   let appContainer = document.getElementById(appID);
+
   // If anything is wrong with the app container then end
   if (!appContainer) {
     console.error("Error: Could not find app contianer" + appID);
